21a5e6a95ed71cfdb664a59b3189a11ea1a0dd5b
fort4 <adcretary@gmail.com>
2025-05-08 10:03:13 +0900

    init: Spring Boot 2.7 project setup

---
+++ src/test/java/io/github/fort4/bootweb/BootwebApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fort4.bootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
